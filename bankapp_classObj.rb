7f16bee01ae86d19c21164abfc9f3e6048a40791
rachelwong <hello.rachelwong@gmail.com>
2019-03-12 14:20:31 +1000

    fixed method calls and object instances

--- bankapp_classObj.rb
+++ bankapp_classObj.rb
@@ -7,7 +7,7 @@ class Account
     attr_accessor :balance
 
     # creates new account 
-    def initialize(name, pin=1234, balance)
+    def initialize(name, pin, balance)
         @name = name
         @pin = pin
         @balance = balance
@@ -15,27 +15,26 @@ class Account
         puts "Press any key to continue."
         STDIN.getch
         print "            \r" # extra space to overwrite in case next sentence is short.   
-        UserInput.new.menu(balance)
     end
 
     # deposit action
     def deposit(amount)
-        if check_value?
-            balance += amount
-            return balance
+        if check_value?(amount)
+            @balance = @balance + amount
+            return @balance
         end
     end
 
     # withdrawal action
     def withdrawal(amount)
-        if check_value? && balance > amount
-            balance -= amount
-            return balance
+        if check_value?(amount) && @balance > amount
+            @balance -= amount
+            return @balance
         end
     end
     
     # display the latest update to balance variable
-    def show_balance(balance)
+    def show_balance()
         puts "Your current balance is #{@balance}."
     end
 
@@ -50,44 +49,58 @@ class Account
 end
 
 class UserInput
-    def menu(balance)
-        puts " ---------- YOUR BALANCE ---------------- "
-        my_account.show_balance(balance)
-        puts " --------------- MENU ------------------- "
-        puts "What would you like to do?"
-        puts "|| {D}eposit || {W}ithdrawal || {B}alance Update || {Q}uit ||"
-        puts    
-        command = gets.chomp.downcase.to_s
-        
-        case command
-        when "d"
-            puts "You have chosen Deposit."
-            puts "How much would you like to deposit:"
-            amount = gets.chomp.to_i
-            my_account.deposit(amount)
-        when "w"
-            puts "You have chosen Withdrawal."
-            puts "How much would you like to withdraw:"
-            amount = gets.chomp.to_i
-            my_account.withdrawal(amount)
-        when "b"
-            puts "You have chosen to see your balance."
-            my_accounts.show_balance(balance)
-        when "q"
-            puts "Good bye!"
-        else
-            error
+    def menu(account)
+
+        puts " ---------- CURRENT BALANCE ---------------- "
+        account.show_balance()
+        puts 
+        puts
+        command = "" 
+        while command != "q"
+            puts " --------------- MENU ------------------- "
+            puts "What would you like to do?"
+            puts "|| {D}eposit || {W}ithdrawal || {B}alance Update || {Q}uit ||"
+            puts   
+    
+            command = gets.chomp.downcase.to_s
+
+            case command
+            when "d"
+                puts "You have chosen Deposit."
+                puts "How much would you like to deposit:"
+                amount = gets.chomp.to_i
+                account.deposit(amount)
+                STDIN.getch
+                print "            \r" # extra space to overwrite in case next sentence is short.           
+                account.show_balance()
+                STDIN.getch
+                print "            \r" # extra space to overwrite in case next sentence is short.           
+            when "w"
+                puts "You have chosen Withdrawal."
+                puts "How much would you like to withdraw:"
+                amount = gets.chomp.to_i
+                account.withdrawal(amount)
+                account.show_balance()
+            when "b"
+                puts "You have chosen to see your balance."
+                account.show_balance()
+            when "q"
+                break
+            else
+                error
+            end
         end
+
     end
 
     def error
         puts "Invalid Input! Please try again."
         STDIN.getch
         print "            \r" # extra space to overwrite in case next sentence is short.   
-        menu(balance)   
     end
 end
 
+# checks the user input pin and name against the stored combination in login.txt
 class PinValidator
     attr_reader :pin
     attr_accessor :name
@@ -101,7 +114,7 @@ class PinValidator
 
         # Matt's idea
         login = false
-
+        person ={}
         for line in File.open("login.txt")
             check = line.split(" ")
             # p check
@@ -112,49 +125,52 @@ class PinValidator
                 break
             end
         end
+
         # Matt's idea
         if login == true
             puts "Log in successful."
-            name = check[0]
-            pin = check[1]
-            balance = check[2].to_i
-            my_account = Account.new(name, pin, balance)
-            my_account.show_balance(balance)
+            person = {name: check[0], pin: check[1], balance: check[2].to_i}
+            return person
         else
             puts "Invalid log in. Good-bye!"
         end
     end
 end
 
-# create a child class to account
-class KidsAccount < Account
-    attr_accessor :consent, :balance, :name
-    attr_reader :pin
-
-    def initialize(name, balance=100, pin, consent)
-        @consent = consent
-        super
-    end
-
-    def withdrawal
-        super
-    end
-
-    def deposit
-        super
-    end
-
-    def show_balance
-        super
-    end
-
-    def get_constent
-    end
-end
-
+# Main Switchboadr for progressing through the program
 def start
     validator = PinValidator.new()
-    validator.check_pin
+    person = validator.check_pin
+    my_account = Account.new(person[:name], person[:pin], person[:balance])
+    userinput1 = UserInput.new()
+    userinput1.menu(my_account)
 end
 
+# Trigger program to start
 start
+
+# create a child class to account
+# class KidsAccount < Account
+#     attr_accessor :consent, :balance, :name
+#     attr_reader :pin
+
+#     def initialize(name, balance=100, pin, consent)
+#         @consent = consent
+#         super
+#     end
+
+#     def withdrawal
+#         super
+#     end
+
+#     def deposit
+#         super
+#     end
+
+#     def show_balance
+#         super
+#     end
+
+#     def get_constent
+#     end
+# end
